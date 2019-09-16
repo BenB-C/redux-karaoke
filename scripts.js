4dd04af27cbda27c41e48f3cf870c75d991fbb17
BenB-C <bcauchon@gmail.com>
2019-09-16 10:53:27 -0700

    Displaying and Mutating Redux State

--- scripts.js
+++ scripts.js
@@ -35,8 +35,26 @@ expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
 // REDUX STORE
 const { createStore } = Redux;
 const store = createStore(reducer);
-console.log('store.getState()', store.getState());
+
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = () => {
+  renderLyrics();
+}
+
 // CLICK LISTENER
 const userClick = () => {
-  console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC' });
 }
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
